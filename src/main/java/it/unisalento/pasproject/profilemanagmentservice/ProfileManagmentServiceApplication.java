dc18ca4c416a4d80f6a41206ade40aa70e9efa90
mirkocaforio <mirkocaforio2002@gmail.com>
2024-05-06 18:33:02 +0200

    First commit

---
+++ src/main/java/it/unisalento/pasproject/profilemanagmentservice/ProfileManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package it.unisalento.pasproject.profilemanagmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileManagmentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unisalento/pasproject/profilemanagmentservice/ProfileManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisalento.pasproject.profilemanagmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
