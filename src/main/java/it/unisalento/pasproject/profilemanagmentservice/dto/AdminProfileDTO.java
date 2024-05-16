241957cf03bb6fca7670391b889bae1835545463
mirkocaforio <mirkocaforio2002@gmail.com>
2024-05-16 11:45:38 +0200

    Add comment

--- src/main/java/it/unisalento/pasproject/profilemanagmentservice/dto/AdminProfileDTO.java
+++ src/main/java/it/unisalento/pasproject/profilemanagmentservice/dto/AdminProfileDTO.java
@@ -3,8 +3,20 @@ package it.unisalento.pasproject.profilemanagmentservice.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * AdminProfileDTO is a data transfer object that extends the GenericProfileDTO.
+ * It is used to transfer data between processes or across network connections.
+ * This class uses Lombok annotations for automatic getter and setter methods.
+ *
+ * @author mirkocaforio
+ * @version 1.0
+ * @since 2024.1.1
+ */
 @Getter
 @Setter
 public class AdminProfileDTO extends GenericProfileDTO {
+    /**
+     * Default constructor for AdminProfileDTO.
+     */
     public AdminProfileDTO() {}
 }
